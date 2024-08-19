2b6298e5e35f59e9cf62cbf3d713aa8dea8c7261
Lea@Mac <leaseep@gmail.com>
2024-08-19 16:14:17 +0200

    add mothers upload

--- program/kallistoSetUp.R
+++ program/kallistoSetUp.R
@@ -1,5 +1,6 @@
 # Setting Up with kallisto input
 
+## Offspring
 allSampleData <- read.csv("../data/SampleData_prepped.csv",row.names = 1)
 allSampleData$ID <- allSampleData$SampleID
 allSampleData$Merged <- as.factor(allSampleData$Condition)
@@ -14,10 +15,10 @@ colnames(tx_anno_df) <- c("GENEID", "TXNAME", "SYMBOL", "GENETYPE")
 #remove all NAs
 tx_anno_df<-tx_anno_df[!is.na(tx_anno_df$TXNAME),]
 
-dds_kallisto <- doKallisto2dds(kallisto_path="/Volumes/My_Book/Seep_Lea/current/fly_sugar/data/output/",
-               sampleTable=allSampleData, # one column needs to be called ID!
-               tx_anno_path=NULL,
-               tx_anno_df=tx_anno_df)
+dds_kallisto <- doKallisto2dds(kallisto_path = "/Volumes/My_Book/Seep_Lea/current/fly_sugar/data/output/",
+               sampleTable = allSampleData, # one column needs to be called ID!
+               tx_anno_path = NULL,
+               tx_anno_df = tx_anno_df)
 
 rowData(dds_kallisto)$GENETYPE <- "none"
 
@@ -27,3 +28,26 @@ for(i in rownames(rowData(dds_kallisto))){
 }
 
 saveRDS(dds_kallisto,"../data/DDS_kallisto_all.rds")
+
+
+## Mothers
+
+motherSampleData <- read.csv("../data/SampleTableMothers.csv",row.names = 1)
+motherSampleData$ID <- motherSampleData$SampleID
+motherSampleData$Condition <- motherSampleData$Diet
+motherSampleData$Merged <- as.factor(motherSampleData$Condition)
+
+
+dds_kallisto_mothers <- doKallisto2dds(kallisto_path = "/Volumes/My_Book/Seep_Lea/current/fly_sugar/data/output/",
+                               sampleTable = motherSampleData, # one column needs to be called ID!
+                               tx_anno_path = NULL,
+                               tx_anno_df = tx_anno_df)
+
+rowData(dds_kallisto_mothers)$GENETYPE <- "none"
+
+for(i in rownames(rowData(dds_kallisto_mothers))){
+  tmp <- unique(subset(tx_anno_df,GENEID==i)$GENETYPE)
+  rowData(dds_kallisto_mothers)[i,"GENETYPE"] <- tmp
+}
+
+saveRDS(dds_kallisto_mothers,"../data/DDS_kallisto_mothers.rds")
